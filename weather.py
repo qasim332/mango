f8fb55f7bad9990d24e23d602d4bc41be8c79dee
qasim332 <muhammadqasim5974@gmail.com>
2020-11-14 11:52:37 +0500

    Humidity added

--- weather.py
+++ weather.py
@@ -1,4 +1,7 @@
 import requests, json
 response = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Lahore,pk&units=metric&appid=1168fedf3708f978364bc3e35b520e83")
 x = response.json()
-print ("The Temprature of Lahore is: " + str(x["main"]["temp"]) + " C")
+
+print("The Temprature of Lahore is: " + str(x["main"]["temp"]) + " C")
+
+print("The Humidity of Lahore is: "+str(x["main"]["humidity"])+" %")
